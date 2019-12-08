2b1df261ced8fc694d52088e7eea72576d87f88b
kai-ono <k.oono.a@gmail.com>
2019-12-08 19:04:21 +0900

    first commit.

---
+++ gatsby-config.js
@@ -0,0 +1,64 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Blog`,
+    description: `Gatsby Blog`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxwidth: 1200,
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/blog/page/1`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plutins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { useEffect } from "react"
+import { navigate } from "gatsby"
+
+const IndexPage = () => {
+  useEffect(() => {
+    navigate("/blog/page/1")
+  }, [])
+  return (
+    <Layout>
+      <SEO title="Home" />
+    </Layout>
+  )
+}
+
+export default IndexPage
